d900f4900b91cbc3ee347b9d0fd0af2c3e6755fc
gunax <gunax@gunax.com>
2019-12-03 21:55:06 -0600

    part 1

--- advent02/src/advent02/core.clj
+++ advent02/src/advent02/core.clj
@@ -1,7 +1,27 @@
 (ns advent02.core
   (:gen-class))
 
+(defn parse
+ "split along commas and return vector of ints"
+[arg]
+(-> arg (.split ",") (#(map read-string %)) vec))
+
+(defn operation
+  "do the opcode at pos n"
+  [ops n]
+  (let [op (get ops n)
+        v1 (get ops (get ops (+ n 1)))
+        v2 (get ops (get ops (+ n 2)))
+        p3 (+ n 3)]
+    (case op
+      99 ops
+      1 (operation (assoc ops (get ops p3) (+ v1 v2)) (+ 4 n))
+      2 (operation (assoc ops (get ops p3) (* v1 v2)) (+ 4 n))
+      )))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Advent 2019 problem 2"
   [& args]
-  (println args))
+  (let [noun (Integer/valueOf (first args))
+        verb (Integer/valueOf (second args))]
+  (print (operation (assoc (parse (slurp "input")) 1 noun 2 verb) 0))))
--- advent02/test/advent02/core_test.clj
+++ advent02/test/advent02/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [advent02.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-1
+  (testing "[1 0 0 0 99]"
+  (is (= [2 0 0 0 99] (operation [1 0 0 0 99] 0)))))
+
+  (deftest test-2
+  (testing "[2 3 0 3 99]"
+  (is (= [2 3 0 6 99] (operation [2 3 0 3 99] 0)))))
+
+  (deftest test-3
+  (testing "[2 4 4 5 99 0]"
+  (is (= [2 4 4 5 99 9801] (operation [2 4 4 5 99 0] 0)))))
+
+  (deftest test-4
+  (testing "[1 1 1 4 99 5 6 0 99]"
+  (is (= [30 1 1 4 2 5 6 0 99] (operation [1 1 1 4 99 5 6 0 99] 0)))))
